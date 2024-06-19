4f0bc36087be4a3006e889e8ed90b01bb3544909
tnakaicode <tnakaicode@gmail.com>
2024-06-19 17:04:34 +0900

    all conversion test

--- tests/test_cadtocsg.py
+++ tests/test_cadtocsg.py
@@ -112,10 +112,10 @@ def test_conversion(input_step_file):
         title="Converted with GEOUNED",
         geometryName=f"{output_filename_stem.resolve()}",
         outFormat=(
-            #"openmc_xml",
-            #"openmc_py",
-            #"serpent",
-            #"phits",
+            # "openmc_xml",
+            # "openmc_py",
+            # "serpent",
+            # "phits",
             "mcnp",
         ),
         volSDEF=True,  # changed from the default
@@ -126,7 +126,110 @@ def test_conversion(input_step_file):
         cellSummaryFile=False,  # changed from the default
     )
 
-    #for suffix in suffixes:
+    # for suffix in suffixes:
+    #    assert output_filename_stem.with_suffix(suffix).exists()
+
+
+def test_conversion_all():
+    """Test that step files can be converted to openmc and mcnp files"""
+
+    my_options = geouned.Options(
+        forceCylinder=False,
+        newSplitPlane=True,
+        delLastNumber=False,
+        enlargeBox=2,
+        nPlaneReverse=0,
+        splitTolerance=0,
+        scaleUp=True,
+        quadricPY=False,
+        Facets=False,
+        prnt3PPlane=False,
+        forceNoOverlap=False,
+    )
+
+    my_tolerances = geouned.Tolerances(
+        relativeTol=False,
+        relativePrecision=0.000001,
+        value=0.000001,
+        distance=0.0001,
+        angle=0.0001,
+        pln_distance=0.0001,
+        pln_angle=0.0001,
+        cyl_distance=0.0001,
+        cyl_angle=0.0001,
+        sph_distance=0.0001,
+        kne_distance=0.0001,
+        kne_angle=0.0001,
+        tor_distance=0.0001,
+        tor_angle=0.0001,
+        min_area=0.01,
+    )
+
+    my_numeric_format = geouned.NumericFormat(
+        P_abc="14.7e",
+        P_d="14.7e",
+        P_xyz="14.7e",
+        S_r="14.7e",
+        S_xyz="14.7e",
+        C_r="12f",
+        C_xyz="12f",
+        K_xyz="13.6e",
+        K_tan2="12f",
+        T_r="14.7e",
+        T_xyz="14.7e",
+        GQ_1to6="18.15f",
+        GQ_7to9="18.15f",
+        GQ_10="18.15f",
+    )
+
+    my_settings = geouned.Settings(
+        matFile="",
+        voidGen=True,
+        debug=False,
+        compSolids=True,
+        simplify="no",
+        exportSolids="",
+        minVoidSize=200.0,  # units mm
+        maxSurf=50,
+        maxBracket=30,
+        voidMat=[],
+        voidExclude=[],
+        startCell=1,
+        startSurf=1,
+        sort_enclosure=False,
+    )
+
+    geo = geouned.CadToCsg(
+        options=my_options,
+        settings=my_settings,
+        tolerances=my_tolerances,
+        numeric_format=my_numeric_format,
+    )
+
+    geo.load_step_file(
+        filename=[f"{f.resolve()}" for f in step_files][0:6], skip_solids=[])
+
+    geo.start()
+
+    geo.export_csg(
+        title="Converted with GEOUNED",
+        geometryName=f"./tests_outputs/ALL",
+        outFormat=(
+            # "openmc_xml",
+            # "openmc_py",
+            # "serpent",
+            # "phits",
+            "mcnp",
+        ),
+        volSDEF=True,  # changed from the default
+        volCARD=False,  # changed from the default
+        UCARD=None,
+        dummyMat=True,  # changed from the default
+        cellCommentFile=False,
+        cellSummaryFile=False,  # changed from the default
+    )
+
+    # for suffix in suffixes:
     #    assert output_filename_stem.with_suffix(suffix).exists()
 
 
